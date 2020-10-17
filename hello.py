74bfb0ee920e226c7380ab105a64f652dd80ae66
johnnyliu <bakolama@gmail.com>
2020-10-17 23:13:46 +0800

    Add Variable Rules
    https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +9,19 @@ def hello_world():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+# string (default) accepts any text without a slash
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)} : {type(username).__name__}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id} : {type(post_id).__name__}' 
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath %s' % escape(subpath)
