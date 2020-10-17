012137bd581269cec32902798bb24b27a5624f2e
johnnyliu <bakolama@gmail.com>
2020-10-17 23:59:50 +0800

    Rendering Templates
    https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 app = Flask(__name__)
 
@@ -6,9 +6,10 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 # string (default) accepts any text without a slash
 @app.route('/user/<username>')
