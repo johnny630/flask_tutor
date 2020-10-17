a29ff025a9c6aba0c5ab53b8e67221628fe1a70e
johnnyliu <bakolama@gmail.com>
2020-10-17 23:23:36 +0800

    Unique URLs / Redirection Behavior
    https://flask.palletsprojects.com/en/1.1.x/quickstart/#unique-urls-redirection-behavior

--- hello.py
+++ hello.py
@@ -25,3 +25,19 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath %s' % escape(subpath)
+
+# The canonical URL for the projects endpoint has a trailing slash. 
+# It’s similar to a folder in a file system. 
+# If you access the URL without a trailing slash, 
+# Flask redirects you to the canonical URL with the trailing slash.
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+# The canonical URL for the about endpoint does not have a trailing slash. 
+# It’s similar to the pathname of a file. Accessing the URL with a trailing 
+# slash produces a 404 “Not Found” error. This helps keep URLs unique for 
+# these resources, which helps search engines avoid indexing the same page twice.
+@app.route('/about')
+def about():
+    return 'The about page'
